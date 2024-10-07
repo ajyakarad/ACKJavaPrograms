7d2b9227a2e8ada09ae8cedd4d6ff374f34f1979
Ajay Karad <karadajay536@gmail.com>
2024-10-07 09:51:52 +0530

    commited chapter 3 programs

--- Chapter1/src/string5.java
+++ Chapter1/src/string5.java
@@ -1,7 +1,7 @@
 public class string5 {
     public static void main(String args[]){
         //string declaration using new keyword
-        String name = new String("rani");
+        String name = new String("ajay");
         System.out.print(name);
     }
 }
